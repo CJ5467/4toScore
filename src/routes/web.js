822b09c6f5e34f0e98682548174295396c6cb9c7
CJ5467 <84459784+CJ5467@users.noreply.github.com>
2022-04-09 13:28:09 -0400

    Add files via upload

---
+++ src/routes/web.js
@@ -0,0 +1,43 @@
+import express from "express";
+import registerController from "../controllers/registerController";
+import loginController from "../controllers/loginController";
+import homePageController from "../controllers/homePageController";
+import userProfileController from "../controllers/userProfileController";
+import staticPagesController from "../controllers/staticPagesController";
+import friendsListController from "../controllers/friendsListController";
+import initPassportLocal from "../controllers/passportLocalController";
+import gameModesController from "../controllers/gameModesController";
+
+/*
+init passport routes
+*/
+initPassportLocal();
+
+let router = express.Router();
+
+let initWebRoutes = (app) => {
+    router.get("/", loginController.checkLoggedIn, homePageController.getHomePage);
+    router.post("/logout", loginController.postLogOut);
+
+    router.get("/register", registerController.getRegisterPage);
+    router.post("/register-new-user", registerController.createNewUser);
+
+    router.get("/login", loginController.checkLoggedOut, loginController.getLoginPage);
+    router.post("/login", loginController.handleLogin);
+
+    //static pages
+    router.get("/howtoplay", loginController.checkLoggedIn, staticPagesController.getHowToPlayPage);
+    router.get("/eloranking", loginController.checkLoggedIn, staticPagesController.getEloRankingPage);
+    router.get("/aboutus", loginController.checkLoggedIn, staticPagesController.getAboutUsPage);
+
+    //game pages
+    router.get("/unranked", loginController.checkLoggedIn, gameModesController.getUnrankedPage);
+    router.get("/ranked", loginController.checkLoggedIn, gameModesController.getRankedPage);
+
+    router.get("/friends", loginController.checkLoggedIn, friendsListController.getFriendsList);
+    router.get("/profile", loginController.checkLoggedIn, userProfileController.getUserProfile);
+    return app.use("/", router);
+};
+
+module.exports = initWebRoutes;
+  
