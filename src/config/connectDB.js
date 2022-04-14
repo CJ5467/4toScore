ac7166f697f664d266977ae363d3092cc5e3cbe7
CJ5467 <84459784+CJ5467@users.noreply.github.com>
2022-04-13 21:13:03 -0400

    Update connectDB.js

--- src/config/connectDB.js
+++ src/config/connectDB.js
@@ -1,16 +1,20 @@
 require("dotenv").config();
 import mysql from "mysql2";
 
-let connection = mysql.createConnection( {
+const connection = mysql.createPool( {
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
     database: process.env.DB_NAME,
     user: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD
+    password: process.env.DB_PASSWORD,
+    waitForConnections: true,
+    connectionLimit: 200,
+    queueLimit: 0
 });
 
+/*
 connection.connect( () => {
     console.log("Mysql connected....");
-});
+});*/
 
 module.exports = connection;
